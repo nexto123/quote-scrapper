f42230be570d6aaf1eca4871ec316b64e8c3a2c5
Ernest Brown <ernestbrown@Ernests-MacBook-Pro.local>
2022-12-19 19:35:49 +0000

    final commit

--- st.py
+++ st.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 
 tag = st.selectbox('choose a topic',['love','humour','life','books'])
 
+generate = st.button('generate csv')
+
 url = f"http://quotes.toscrape.com/tag/{tag}/"
 
 #
@@ -14,4 +16,24 @@ res = requests.get(url)
 
 content = BeautifulSoup(res.content, 'html.parser')
 
-st.code(content)
+quotes  = content.find_all('div',class_='quote')
+
+quote_file = []
+
+for quote in quotes:
+    text  = quote.find('span', class_='text').text
+    author = quote.find('small', class_='author').text
+    link = quote.find('a')
+    st.success(text)
+    st.write(author)
+    st.markdown(f"<a href=http://quotes.toscrape.com{link['href']}>{author}</a>", unsafe_allow_html=True )
+    st.code(f"http://quotes.toscrape.com{link['href']}")
+    quote_file.append([text, author, link])
+
+
+if generate:
+    try:
+        df = pd.DataFrame(quote_file)
+        df.to_csv('quote.csv', index=False, header=['Quote','Author','Link'])
+    except:
+        st.write('loading..')
